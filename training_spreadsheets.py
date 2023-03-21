35ea8d6b5a8a5eb42fe0628e96aa6f6847684fc8
WorkHotRoad <dima@ezoport.com>
2023-03-21 23:44:17 +0300

    release_v1

---
+++ training_spreadsheets.py
@@ -0,0 +1,83 @@
+import os
+
+from google.oauth2.service_account import Credentials
+from googleapiclient import discovery
+from dotenv import load_dotenv
+
+
+SCOPES = [
+         'https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive',
+]
+
+load_dotenv()
+
+EMAIL_USER = os.environ['EMAIL']
+
+info = {
+    'type':  os.environ['TYPE'],
+    'project_id':  os.environ['PROJECT_ID'],
+    'private_key_id':  os.environ['PRIVATE_KEY_ID'],
+    'private_key':  os.environ['PRIVATE_KEY'],
+    'client_email':  os.environ['CLIENT_EMAIL'],
+    'client_id':  os.environ['CLIENT_ID'],
+    'auth_uri':  os.environ['AUTH_URI'],
+    'token_uri':  os.environ['TOKEN_URI'],
+    'auth_provider_x509_cert_url':  os.environ['AUTH_PROVIDER_X509_CERT_URL'],
+    'client_x509_cert_url':  os.environ['CLIENT_X509_CERT_URL']
+}
+
+def auth():
+    # Создаём экземпляр класса Credentials.
+    credentials = Credentials.from_service_account_info(
+        info=info, scopes=SCOPES)
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+    return service, credentials
+
+def create_spreadsheet(service):
+    # Тело spreadsheet
+    spreadsheet_body = {
+         # Свойства документа
+        'properties': {
+            'title': 'Бюджет путешествий',
+            'locale': 'ru_RU'
+        },
+        # Свойства листов документа
+        'sheets': [{
+            'properties': {
+                'sheetType': 'GRID',
+                'sheetId': 0,
+                'title': 'Отпуск 2077',
+                'gridProperties': {
+                    'rowCount': 100,
+                    'columnCount': 100
+                }
+             }
+         }]
+    }
+    request = service.spreadsheets().create(body=spreadsheet_body)
+    response = request.execute()
+    spreadsheet_id = response['spreadsheetId']
+
+    print('https://docs.google.com/spreadsheets/d/' + spreadsheet_id)
+    return spreadsheet_id
+
+def set_user_permissions(spreadsheet_id, credentials):
+    permissions_body={'type': 'user', # Тип учетных данных.
+                      'role': 'writer', # Права доступа для учётной записи.
+                      'emailAddress': EMAIL_USER} # Ваш личный гугл-аккаунт.
+
+    # Создаётся экземпляр класса Resource для Google Drive API.
+    drive_service = discovery.build('drive', 'v3', credentials=credentials)
+
+    # Формируется и сразу выполняется запрос на выдачу прав вашему аккаунту.
+    drive_service.permissions().create(
+        fileId=spreadsheet_id,
+        body=permissions_body,
+        fields='id'
+    ).execute()
+
+
+service, credentials = auth()
+spreadsheetId = create_spreadsheet(service)
+set_user_permissions(spreadsheetId, credentials)
